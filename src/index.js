6ac9c9e34f6ebb0073841c090f555ec4e5d16fd3
Joaco <joacopotenzone@hotmail.com>
2023-11-22 11:52:50 -0300

    agregando json de products. y configurando el productManager

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+const ProductManager = require("./productManager");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const app = express();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+  res.status(201).send("Welcome to Esto no es Moda!");
+});
+
+
+const productManager = new ProductManager("./src/products.json");
+
+app.get("/products", async (req, res) => {
+  console.log(await productManager.getProducts());
+  let limit = req.query.limit;
+  const returnProducts = await productManager.getProducts();
+  if (limit) {
+    res.status(200).json({ status: "ok", data: returnProducts.slice(0, limit) });
+  } else {
+    res.status(200).json({ status: "ok", data: returnProducts });
+  }
+});
+
+app.get("/products/:pid", async (req, res) => {
+  try {
+    const id = parseInt(req.params.pid);
+    const product = await productManager.getProductById(id);
+    res.status(200).json({ status: "ok", data: product });
+  } catch (error) {
+    res.status(404).json({ status: "error", message: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port http:/localhost:${PORT}`);
+});
